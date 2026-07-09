c65927bc089d6c423afbe95e057301c2c649bf82
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-09 13:22:54 +0500

    User Class is Registered.

--- eventHandling/authApp/admin.py
+++ eventHandling/authApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from authApp.models import Role
+from authApp.models import Role, User
 
 # Register your models here.
 admin.site.register(Role)
+admin.site.register(User)
