373bbfe188bee28d3993c03966ac1e5c2bf72cf1
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-09 13:23:25 +0500

    User Class is Created

--- eventHandling/authApp/models.py
+++ eventHandling/authApp/models.py
@@ -3,3 +3,16 @@ from django.db import models
 # Create your models here.
 class Role(models.Model):
     role_name = models.CharField(max_length=100, unique=True, null=False, blank=False, help_text="Enter the name of the role", choices=[('isAdmin', 'Admin'), ('isUser', 'User')])
+
+    def __str__(self):
+        return self.role_name
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True, null=False, blank=False, help_text="Enter the username")
+    email = models.EmailField(unique=True, null=False, blank=False, help_text="Enter the email address")
+    password = models.CharField(max_length=100, null=False, blank=False, help_text="Enter the password")
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, related_name='users', help_text="Select the role for the user")
+
+    def __str__(self):
+        return self.username
