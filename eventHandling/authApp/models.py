05d86635e5ed5803ac5217e11ba0f3d10ac1f89e
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-09 13:10:20 +0500

    Models are being created

--- eventHandling/authApp/models.py
+++ eventHandling/authApp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Role(models.Model):
+    role_name = models.CharField(max_length=100, unique=True, null=False, blank=False, help_text="Enter the name of the role", choices=[('isAdmin', 'Admin'), ('isUser', 'User')])
