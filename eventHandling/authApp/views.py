baf2b85014d6c959c7426f88df1a7433c0b473ee
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-10 21:44:10 +0500

    Minor Changes in Views.py[authApp]

--- eventHandling/authApp/views.py
+++ eventHandling/authApp/views.py
@@ -9,22 +9,26 @@ from authApp.models import Role, User
 # Create your views here.
 @csrf_exempt
 def register_user(request):
-    if request.method == 'POST':
-        setData = json.loads(request.body)
-        # print(setData)
-        userName = setData.get('user_name')
-        email = setData.get('email')
-        password = setData.get('pass')
-        role = setData.get('role')
-        userRole = role
-        user_role = Role.objects.filter(role_name=userRole)
-        if user_role.exists():
-            user_role = user_role.first(); 
-        else:
-            user_role = Role.objects.create(role_name=userRole)
-            user_role.save();
-        user = User.objects.create(username=userName, email=email, password=password, role=user_role)
-        if user is not None: 
-            user.save();
-        return JsonResponse({'message': 'User registered successfully'})
+    try: 
+        if request.method == 'POST':
+            setData = json.loads(request.body)
+            # print(setData)
+            userName = setData.get('user_name')
+            email = setData.get('email')
+            password = setData.get('pass')
+            role = setData.get('role')
+            userRole = role
+            user_role = Role.objects.filter(role_name=userRole)
+            if user_role.exists():
+                user_role = user_role.first(); 
+            else:
+                user_role = Role.objects.create(role_name=userRole)
+                user_role.save();
+            user = User.objects.create(username=userName, email=email, password=password, role=user_role)
+            if user is not None: 
+                user.save();
+            return JsonResponse({'message': 'User registered successfully', 'userName':user.username})
+    except json.JSONDecodeError as error:
+        return JsonResponse({'message': 'Error Loading the Json',
+                             'status': error})
     return JsonResponse({'message': 'Working'})
