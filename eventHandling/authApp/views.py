605e3874a3ad649743492f816da5e522aed2b00c
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-10 21:48:47 +0500

    Comments Added in Views.py[authApp] (register fn)

--- eventHandling/authApp/views.py
+++ eventHandling/authApp/views.py
@@ -11,24 +11,24 @@ from authApp.models import Role, User
 def register_user(request):
     try: 
         if request.method == 'POST':
-            setData = json.loads(request.body)
+            setData = json.loads(request.body) # Loading the JSON
             # print(setData)
             userName = setData.get('user_name')
             email = setData.get('email')
             password = setData.get('pass')
             role = setData.get('role')
-            userRole = role
-            user_role = Role.objects.filter(role_name=userRole)
+            userRole = role # Assigning the Values that are entered by the user.
+            user_role = Role.objects.filter(role_name=userRole)  # Filtering the Data whether found or not as written above
             if user_role.exists():
-                user_role = user_role.first(); 
+                user_role = user_role.first();  # If the Role is found in the database it will be then passed to the user_role which will later save to the User Table:
             else:
-                user_role = Role.objects.create(role_name=userRole)
+                user_role = Role.objects.create(role_name=userRole) # Otherwise new role is created, if failed to fetch from the database:
                 user_role.save();
             user = User.objects.create(username=userName, email=email, password=password, role=user_role)
             if user is not None: 
                 user.save();
-            return JsonResponse({'message': 'User registered successfully', 'userName':user.username})
+            return JsonResponse({'message': 'User registered successfully', 'userName':user.username}) # Returning The JSON response once succeeded.
     except json.JSONDecodeError as error:
         return JsonResponse({'message': 'Error Loading the Json',
-                             'status': error})
+                             'status': str(error)}) # Error Wrapped in String:
     return JsonResponse({'message': 'Working'})
