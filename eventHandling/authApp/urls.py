e8786097b5e7d96a231af2c8cf5f13a42714b80e
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-10 22:53:51 +0500

    New Path is Configured in URLS file[authApp]

--- eventHandling/authApp/urls.py
+++ eventHandling/authApp/urls.py
@@ -4,5 +4,6 @@ from authApp import views
 urlpatterns = [
     # Add URL patterns of authApp.
     path('register/', views.register_user, name='register_user'), 
-    path('login/', views.login_user, name='login_user')
+    path('login/', views.login_user, name='login_user'),
+    path('logout/', views.logout_user, name='logout_user')
 ]
