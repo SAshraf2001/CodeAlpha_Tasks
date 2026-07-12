88aa6e873fe7b8a8116201d910123e649f8ecc32
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-12 22:50:27 +0500

    Models(eventHandle) are Registered

--- eventHandling/eventHandle/admin.py
+++ eventHandling/eventHandle/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from eventHandle.models import UserRegister, EventRegister
 
 # Register your models here.
+admin.site.register(UserRegister);
+admin.site.register(EventRegister);
