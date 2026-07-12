6b04bbc0ae67842b037dab14de93228eb2d30423
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-12 23:11:31 +0500

    Testing Functions and URLS bridge

--- eventHandling/eventHandle/views.py
+++ eventHandling/eventHandle/views.py
@@ -1,24 +1,41 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from eventHandle.models import EventRegister, UserRegister
+from django.utils import timezone
 
 # Create your views here.
 
 def user_register(request):
     # User Registeration before Event Placing:
-    pass
+    return JsonResponse({
+        'status': 'Passed',
+        'message': "User Registeration URL is working completely:"
+    })
 
 def event_registeration(request):
     # Events Registeration
-    pass
-
+    return JsonResponse({
+        'status': 'Passed',
+        'message': "Event Registeration URL is working completely:"
+    })
 
 def event_list(request):
     # All The Events are listed.
-    pass
-
+    return JsonResponse({
+        'status': 'Passed',
+        'message': "Events Listing URL is working completely:"
+    })
 def user_list(request):
     # All the users fetched.
-    pass
+    return JsonResponse({
+        'status': 'Passed',
+        'message': "User Listing URL is working completely:"
+    })
 
 def event_handle(request):
     # Event Handling:
-    pass
+    return JsonResponse({
+        'status': 'Passed',
+        'message': "Event Handling URL is working completely:"
+    })
