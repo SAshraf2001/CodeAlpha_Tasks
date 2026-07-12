8959479e4d4d3fad6a5c87fe56af0f50d0e5145e
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-12 22:57:09 +0500

    Patterns are Added

--- eventHandling/eventHandle/urls.py
+++ eventHandling/eventHandle/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from eventHandle.views import user_register, event_registeration, event_list, user_list, event_handle
 
 urlpatterns = [
     # Adding all the Event Handling app URL patterns here.
