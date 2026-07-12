108950ab51c49127f259cc7559f3aaf5511a9b6e
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-12 23:00:06 +0500

    Paths are Added ---> eventHandle

--- eventHandling/eventHandle/urls.py
+++ eventHandling/eventHandle/urls.py
@@ -3,5 +3,9 @@ from eventHandle.views import user_register, event_registeration, event_list, us
 
 urlpatterns = [
     # Adding all the Event Handling app URL patterns here.
-    
+    path('userRegister/', user_register, name='user_register'),
+    path('event/', event_registeration, name='event_registeration'),
+    path('list/', event_list, name='event_list'),
+    path('userList/', user_list, name='user_list'),
+    path('e-handle/', event_handle, name='event_handle')
 ]
