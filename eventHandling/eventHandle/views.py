917df4c6dfd0c02c0b66273021d84d18a2aabb31
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-12 22:55:29 +0500

    Functions for Event Handling.

--- eventHandling/eventHandle/views.py
+++ eventHandling/eventHandle/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def user_register(request):
+    # User Registeration before Event Placing:
+    pass
+
+def event_registeration(request):
+    # Events Registeration
+    pass
+
+
+def event_list(request):
+    # All The Events are listed.
+    pass
+
+def user_list(request):
+    # All the users fetched.
+    pass
+
+def event_handle(request):
+    # Event Handling:
+    pass
