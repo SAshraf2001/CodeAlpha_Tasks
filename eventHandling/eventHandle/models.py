92db73b9535364e9b990e7e6c8901a0033e59ea6
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-13 23:28:03 +0500

    Changes in Models.py File

--- eventHandling/eventHandle/models.py
+++ eventHandling/eventHandle/models.py
@@ -20,8 +20,15 @@ class UserRegister(models.Model):
     lastName = models.CharField(max_length=100, null=False, blank=False, help_text='Enter your Last Name:')
     address = models.CharField(max_length=100, null=False, blank=False, help_text='Enter your Address')
     phoneNumber = models.CharField(max_length=15, null=False, blank=False, help_text='Enter Contact Number')
-    registeredUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
-    userTicket = models.ForeignKey(EventRegister, on_delete=models.CASCADE, related_name='Tickets')
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='AuthUser')
+
     def __str__(self):
         return self.firstName;
+    
+class EventHandle(models.Model):
+    seatingCapacity = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    registeredUser = models.ForeignKey(UserRegister, on_delete=models.CASCADE, related_name='users')
+    userTicket = models.ForeignKey(EventRegister, on_delete=models.CASCADE, related_name='Event')
+    
+    def __str__(self):
+        return f'{self.userTicket.title}'
