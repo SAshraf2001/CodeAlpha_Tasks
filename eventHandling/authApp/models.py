5b1e766992753b84db5102657c220d66fac589a5
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-14 12:41:10 +0500

    Small Typos Fixed

--- eventHandling/authApp/models.py
+++ eventHandling/authApp/models.py
@@ -13,7 +13,7 @@ class User(AbstractUser):
     username = models.CharField(max_length=100, unique=True, null=False, blank=False, help_text="Enter the username")
     email = models.EmailField(unique=True, null=False, blank=False, help_text="Enter the email address")
     password = models.CharField(max_length=100, null=False, blank=False, help_text="Enter the password")
-    role = models.ForeignKey(Role, on_delete=models.CASCADE, related_name='users', help_text="Select the role for the user")
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, null=True, blank=True, related_name='users', help_text="Select the role for the user")
     REQUIRED_FIELDS = ['email', 'role']
     def __str__(self):
         return self.username
