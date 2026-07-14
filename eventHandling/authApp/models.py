08fba44eafb50ad3e0295f1bafbea89f5b7a5877
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-14 12:37:30 +0500

    Small Changes in User Model(authApp)

--- eventHandling/authApp/models.py
+++ eventHandling/authApp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Role(models.Model):
-    role_name = models.CharField(max_length=100, unique=True, null=False, blank=False, help_text="Enter the name of the role", choices=[('isAdmin', 'Admin'), ('isUser', 'User')])
+    role_name = models.CharField(max_length=100, unique=True, null=False, blank=False, help_text="Enter the name of the role", choices=[('isAdmin', 'Admin'), ('isUser', 'User')], default='Admin')
 
     def __str__(self):
         return self.role_name
@@ -14,6 +14,6 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, null=False, blank=False, help_text="Enter the email address")
     password = models.CharField(max_length=100, null=False, blank=False, help_text="Enter the password")
     role = models.ForeignKey(Role, on_delete=models.CASCADE, related_name='users', help_text="Select the role for the user")
-    REQUIRED_FIELDS = ['email']
+    REQUIRED_FIELDS = ['email', 'role']
     def __str__(self):
         return self.username
