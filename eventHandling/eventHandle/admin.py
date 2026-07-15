0ce5323f3e0da794b7275f730d7e48396a770b21
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-15 23:55:04 +0500

    Admin.py File is Updated

--- eventHandling/eventHandle/admin.py
+++ eventHandling/eventHandle/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from eventHandle.models import UserRegister, EventRegister
 
-# Register your models here.
-admin.site.register(UserRegister);
-admin.site.register(EventRegister);
+# Register UserRegister simply
+admin.site.register(UserRegister)
+
+# Register EventRegister with your custom class using the decorator
+@admin.register(EventRegister)
+class EventRegisterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'place', 'date', 'capacity')
