cab7067a5924c5715a756728857709f3879d73b8
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-15 12:01:22 +0500

    Typose Fixed

--- eventHandling/eventHandle/views.py
+++ eventHandling/eventHandle/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from eventHandle.models import EventRegister, UserRegister
