1e719ab61e7dc54d73c69c20f537d18065764b50
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-16 11:23:08 +0500

    EventHandle Model has been Registered

--- eventHandling/eventHandle/admin.py
+++ eventHandling/eventHandle/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from eventHandle.models import UserRegister, EventRegister
+from eventHandle.models import UserRegister, EventRegister, EventHandle
 
 # Register UserRegister simply
 admin.site.register(UserRegister)
@@ -8,3 +8,7 @@ admin.site.register(UserRegister)
 @admin.register(EventRegister)
 class EventRegisterAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'place', 'date', 'capacity')
+    
+@admin.register(EventHandle)
+class EventHandleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'seatingCapacity', 'registeredUser', 'userTicket')
