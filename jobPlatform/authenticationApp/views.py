05c9da1be4d1f3a3ddba94df346ce9901838305a
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-20 22:31:30 +0500

    Views.py Testing

--- jobPlatform/authenticationApp/views.py
+++ jobPlatform/authenticationApp/views.py
@@ -1,3 +1,11 @@
+import json
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+
+
+def home_view(request):
+    return JsonResponse({
+        'Status': 'Passed and Completed',
+        'Message': 'URL is working.'
+    })
