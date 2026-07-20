b06b899e2170dd67f24383ac8d8486a484d6e6f7
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-20 11:06:00 +0500

    Database is configured and Connected

--- jobPlatform/jobPlatform/settings.py
+++ jobPlatform/jobPlatform/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'jobPlatform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_job_platform',
+        'USER': 'event_user',
+        'PASSWORD': '2662',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
