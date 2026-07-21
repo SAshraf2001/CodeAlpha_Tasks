836b63c6ba380706ec17e36d58c2aa8670b06620
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-21 12:01:39 +0500

    Database Table Schemas for jobPlatform User

--- jobPlatform/authenticationApp/models.py
+++ jobPlatform/authenticationApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Role(models.Model):
+    roleName = models.CharField(max_length=100, unique=True, null=False, blank=False, help_text="Enter the Desired Role for the User:", choices=[('isAdmin', 'Admin'), ('isRecruiter', 'Recruiter'), ('isEmployee', 'Employee')], default='Admin')
+    
+    def __str__(self):
+        return self.roleName;
+
+class User(AbstractUser):
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, null=True, blank=True, related_name='users', help_text='Select the Desired Role for the User:')
+    
+    REQUIRED_FIELDS=['email']
+    
+    def __str__(self):
+        return self.userName
