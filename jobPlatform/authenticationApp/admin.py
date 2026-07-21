ad367b03b2aff63224a46ce2bc057f6a45a8f08a
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-21 12:01:05 +0500

    All the Models are Registered

--- jobPlatform/authenticationApp/admin.py
+++ jobPlatform/authenticationApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from authenticationApp.models import Role, User
 
 # Register your models here.
+@admin.register(Role)
+class roleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'roleName')
+    
+    
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [('role')]
