0359fbe23d7a665ca54c977a45eaf543acd6e6f3
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-21 21:05:11 +0500

    Path for Logout Function Configured

--- jobPlatform/authenticationApp/urls.py
+++ jobPlatform/authenticationApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from authenticationApp.views import registeredUser, loginView
+from authenticationApp.views import registeredUser, loginView, logoutUser
 
 urlpatterns = [
     path('sign-up/', registeredUser, name='registeredUser'),
-    path('login/', loginView, name='loginView')
+    path('login/', loginView, name='loginView'),
+    path('sign-out/', logoutUser, name='logoutUser')
 ]
