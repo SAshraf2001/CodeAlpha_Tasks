fee8a28f04beebbfd97b1518a1332a6d3c27a4c6
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-21 13:57:29 +0500

    Login URL is Fixed

--- jobPlatform/authenticationApp/urls.py
+++ jobPlatform/authenticationApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from authenticationApp.views import registeredUser
+from authenticationApp.views import registeredUser, loginView
 
 urlpatterns = [
-    path('sign-up/', registeredUser, name='registeredUser')
+    path('sign-up/', registeredUser, name='registeredUser'),
+    path('login/', loginView, name='loginView')
 ]
