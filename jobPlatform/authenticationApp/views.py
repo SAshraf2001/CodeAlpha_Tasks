593a4d8defe75ff1a62cfa729fda0614c11d3138
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-21 13:19:51 +0500

    Registered User Function

--- jobPlatform/authenticationApp/views.py
+++ jobPlatform/authenticationApp/views.py
@@ -6,6 +6,37 @@ from django.http import JsonResponse
 
 
 def registeredUser(request):
+    try:
+        if request.method == 'POST':
+            setData = json.loads(request.body)
+            userName = setData.get('User Name')
+            email = setData.get('email')
+            password = setData.get('Password')
+            userRole = setData.get('User Role')
+            
+            user_role = Role.objects.filter(roleName = userRole)
+            if user_role.exists():
+                user_role = user_role.first()
+            else: 
+                user_role = Role.objects.create(roleName=userRole)
+            user = User.objects.create(role=user_role)
+            user.username = userName # Extracting the User Name from the Input and passing it to the builtIn username.
+            user.email = email
+            user.set_password(password)
+            user.save()
+            
+            return JsonResponse({
+                'Status': 'User is Registered',
+                'User Name': userName,
+                'Email': email,
+                'Role': userRole
+            })
+    except json.JSONDecodeError as err:
+        return JsonResponse({
+            'Status': 'Failed',
+            'Message': f"Exception Caught ---> Error:{str(err)}"
+        })    
+        
     return JsonResponse({
         'Status': 'Passed and Completed',
         'Message': 'URL is working.'
