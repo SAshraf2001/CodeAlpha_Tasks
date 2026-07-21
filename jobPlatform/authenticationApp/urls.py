909b324f6cdfa658957e1694f31ff38a7fee08ab
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-21 13:19:04 +0500

    Small Error Fixed

--- jobPlatform/authenticationApp/urls.py
+++ jobPlatform/authenticationApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 
 from authenticationApp.views import registeredUser
 
-url_patterns = [
+urlpatterns = [
     path('sign-up/', registeredUser, name='registeredUser')
 ]
