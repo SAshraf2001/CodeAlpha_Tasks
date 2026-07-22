1b5e5b6ed99860741746b6abd5bb9e569347dea5
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-22 23:06:07 +0500

    Models Registered

--- jobPlatform/jobPosting/admin.py
+++ jobPlatform/jobPosting/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from jobPosting.models import UserProfile, jobPosting
 
 # Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'First Name', 'Last Name')
+    
+
+@admin.regist(jobPosting)
+class jobPostingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Title', 'Job Description')
