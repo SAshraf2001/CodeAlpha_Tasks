1f4f22edfbcc48b8996d29df882245c390110e85
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-22 23:17:49 +0500

    New Schema Added

--- jobPlatform/jobPosting/models.py
+++ jobPlatform/jobPosting/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+from authenticationApp.models import User
+
+from django.utils import timezone
 
 # Create your models here.
 class UserProfile(models.Model):
-    pass
-
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    address = models.CharField(max_length=255, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    
+    resume = models.FileField(upload_to='resumes/', blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    portfolio_url = models.URLField(blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+    
+    
 class jobPosting(models.Model):
     pass
