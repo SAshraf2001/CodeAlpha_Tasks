a436f820f80c2e56119e968cafe362d12cf2355f
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-22 22:28:46 +0500

    User Registeration Function is Created

--- jobPlatform/jobPosting/views.py
+++ jobPlatform/jobPosting/views.py
@@ -1,3 +1,19 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def userRegisteration(request):
+    try:
+        if request.method == 'POST':
+            pass
+    except json.JSONDecodeError as err:
+        return JsonResponse({
+            'Status': 'Failed',
+            'Message': f"Exception Caught: Error Debugged ---> {str(err)}"
+        })
+    
+    return JsonResponse({
+        'Status': 'Passed',
+        'Message': "URL is Working Fine"
+    })
