d8ce7a0668e4a37f47e7eb93bde95035f3f650ec
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-26 22:51:19 +0500

    User Function is Created

--- jobPlatform/jobPosting/views.py
+++ jobPlatform/jobPosting/views.py
@@ -1,22 +1,31 @@
 import json
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import JsonResponse
+from jobPosting.models import UserProfile, JobStatus, jobPosting, EmployeeType
 
 # Create your views here.
+@csrf_exempt
 def userRegisteration(request):
-    try:
-        if request.method == 'POST':
-            pass
-    except json.JSONDecodeError as err:
-        return JsonResponse({
-            'Status': 'Failed',
-            'Message': f"Exception Caught: Error Debugged ---> {str(err)}"
-        })
-    
-    return JsonResponse({
-        'Status': 'Passed',
-        'Message': "URL is Working Fine"
-    })
+    if request.method == 'POST':
+        logged_user = request.user
+        address = request.POST['Address']
+        contactNumber = request.POST['Contact Number']
+        resume = request.FILES['Resume']
+        userBio = request.POST['BIO']
+        profilePicture = request.FILES['Profile Picture']
+        portfolio = request.POST['URL']
+            
+        if ((address) and (contactNumber) and (resume) and (userBio) and (profilePicture) and (portfolio) and (logged_user)):    
+            UserProfile.objects.create(user=logged_user, address=address, phone_number=contactNumber, resume=resume, profile_picture=profilePicture, bio=userBio, portfolio_url = portfolio)
+            
+            return JsonResponse({
+                'Status': 'Saved Successfully',
+                'Name': logged_user.username,
+                'Address': address,
+                'Phone Number': contactNumber,
+                'User Bio': userBio,
+            })
     
 
 def create_job_posting(request):
