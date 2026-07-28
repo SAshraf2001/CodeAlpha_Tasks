36c1dbbf7d99bd1ccce09961e9a631f615152d75
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-28 22:33:30 +0500

    URL linked to upload_resume is Deleted

--- jobPlatform/jobPosting/urls.py
+++ jobPlatform/jobPosting/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from jobPosting.views import userRegisteration, create_job_posting, upload_resumes, track_application, apply_job, search_job_listings
+from jobPosting.views import userRegisteration, create_job_posting, track_application, apply_job, search_job_listings
 
 urlpatterns = [
     path('user/', userRegisteration, name='userRegisteration'),
     path('apply/', apply_job, name='apply_job'),
     path('search/', search_job_listings, name='search_job_listings'),
     path('create/', create_job_posting, name='create_job_posting'),
-    path('track/', track_application, name='track_application'),
-    path('resume/', upload_resumes, name='upload_resumes')
+    path('track/', track_application, name='track_application')
 ]
