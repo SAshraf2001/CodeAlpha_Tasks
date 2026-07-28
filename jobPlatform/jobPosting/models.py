270cf2fb506f0381beccb9c9d457b88178e3835f
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-28 10:54:43 +0500

    Typos Fixed

--- jobPlatform/jobPosting/models.py
+++ jobPlatform/jobPosting/models.py
@@ -52,5 +52,6 @@ class jobApply(models.Model):
     jobUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name='applicants')
     jobName = models.CharField(max_length=100, blank=False, null=False, help_text='Enter the Desired Job Name')
     resume_upload = models.FileField(upload_to = 'resume-uploads/', blank=False, null=False)
+    
     def __str__(self):
         return self.jobName
